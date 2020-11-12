5510869bf68bd454909729e2c3da2e3e5140c655
Drubico <00121117@uca.edu.sv>
2020-11-12 16:28:32 -0600

    Modificado y

--- functions/y.cpp
+++ functions/y.cpp
@@ -67,10 +67,9 @@ bool MatrixFivePos(int** matrix, int posR,int posC,char pos){
 /*
 
     char matrix[16][16]={{'a','b','c','c','c','c','c','c','a','b','c','c','c','c','c','c'},{'a','b','c','c','c','c','c','c','a','b','c','c','c','c','c','c'},{'a','b','c','c','c','c','c','c','a','b','c','c','c','c','c','c'},{'a','b','c','c','c','c','c','c','a','b','c','c','c','c','c','c'},{'a','b','c','c','c','c','c','c','a','b','c','c','c','c','c','c'},{'a','b','c','c','c','c','c','c','a','b','c','c','c','c','c','c'},{'a','b','c','c','c','c','c','c','a','b','c','c','c','c','c','c'},{'a','b','c','c','c','c','c','c','a','b','c','c','c','c','c','c'},{'a','b','c','c','c','c','c','c','a','b','c','c','c','c','c','c'},{'a','b','c','c','c','c','c','c','a','b','c','c','c','c','c','c'},{'a','b','c','c','c','c','c','c','a','b','c','c','c','c','c','c'},{'a','b','c','c','c','c','c','c','a','b','c','c','c','c','c','c'},{'a','b','c','c','c','c','c','c','a','b','c','c','c','c','c','c'},{'a','b','c','c','c','c','c','c','a','b','c','c','c','c','c','c'},{'a','b','c','c','c','c','c','c','a','b','c','c','c','c','c','c'},{'a','b','c','c','c','c','c','c','a','b','c','c','c','c','c','c'}};
-    
     for(int i = 0; i < 16; i++){
         for(int j = 0; j < 16; j++)
-            cout<<((MatrixFivePos(matrix,i,j)==false)? "No":"Yes")<<"\t";
+            cout<<((MatrixFivePos(matrix,i,j,'a')==false)? "No":"Yes")<<"\t";
         cout<<"\n";
     }
 
